fbc68abd1400aaf2dc79aea6e4a5ca592f8d3234
Tim Lucas <t@toolmantim.com>
2019-02-03 23:00:06 +1100

    Trigger action

---
+++ trigger-buildkite-pipeline/trigger.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [[ -z "$BUILDKITE_API_ACCESS_TOKEN" ]]; then
+  echo "You must set the BUILDKITE_API_ACCESS_TOKEN environment variable (e.g. BUILDKITE_API_ACCESS_TOKEN = \"xyz\")"
+  exit 1
+fi
+
+if [[ -z "$BUILDKITE_PIPELINE" ]]; then
+  echo "You must set the BUILDKITE_PIPELINE environment variable (e.g. BUILDKITE_PIPELINE = \"my-org/my-pipeline\")"
+  exit 1
+fi
+
+BUILDKITE_ORG_SLUG=$(echo "$BUILDKITE_PIPELINE" | cut -d'/' -f1)
+BUILDKITE_PIPELINE_SLUG=$(echo "$BUILDKITE_PIPELINE" | cut -d'/' -f2)
+
+curl \
+  -X POST \
+  -H "Authorization: Bearer ${BUILDKITE_API_ACCESS_TOKEN}" \
+  "https://api.buildkite.com/v2/organizations/${BUILDKITE_ORG_SLUG}/pipelines/${BUILDKITE_PIPELINE_SLUG}/builds" \
+  -d "{
+    \"commit": "${GITHUB_SHA}\",
+    \"branch": "${GITHUB_REF}\"
+  }"
