0e12cf241bbd01b7e51b12d01aa49f0235a2da80
Tim Lucas <t@toolmantim.com>
2019-02-04 22:38:53 +1100

    Set the author of the build

--- trigger-buildkite-pipeline/trigger.sh
+++ trigger-buildkite-pipeline/trigger.sh
@@ -18,14 +18,30 @@ COMMIT="${BUILDKITE_COMMIT:-${GITHUB_SHA}}"
 BRANCH="${BUILDKITE_BRANCH:-${GITHUB_REF#"refs/heads/"}}"
 MESSAGE="${BUILDKITE_MESSAGE:-}"
 
+NAME=$(jq ".pusher.name" "$GITHUB_EVENT_PATH")
+EMAIL=$(jq ".pusher.email" "$GITHUB_EVENT_PATH")
+
+# Use jq’s --arg properly escapes string values for us
+JSON=$(
+  jq -n \
+    --arg COMMIT  "$COMMIT" \
+    --arg BRANCH  "$BRANCH" \
+    --arg MESSAGE "$MESSAGE" \
+    --arg NAME    "$NAME" \
+    --arg EMAIL   "$EMAIL" \
+    '{
+      "commit": $COMMIT,
+      "branch": $BRANCH,
+      "message": $MESSAGE,
+      "author": {
+        "name": $NAME,
+        "email": $EMAIL
+      }
+    }'
+)
+
 curl \
   -X POST \
   -H "Authorization: Bearer ${BUILDKITE_API_ACCESS_TOKEN}" \
   "https://api.buildkite.com/v2/organizations/${ORG_SLUG}/pipelines/${PIPELINE_SLUG}/builds" \
-  -d @- << JSON
-{
-  "commit": "${COMMIT}",
-  "branch": "${BRANCH}",
-  "message": "${MESSAGE}"
-}
-JSON
+  -d "$JSON"
