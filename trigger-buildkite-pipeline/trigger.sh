ca2cd88165affe8bb1ec1c5a33f1c081c705a0f0
Tim Lucas <t@toolmantim.com>
2019-02-04 14:03:34 +1100

    Allow message and fix pipeline

--- trigger-buildkite-pipeline/trigger.sh
+++ trigger-buildkite-pipeline/trigger.sh
@@ -7,7 +7,7 @@ if [[ -z "${BUILDKITE_API_ACCESS_TOKEN:-}" ]]; then
   exit 1
 fi
 
-if [[ -z "${PIPELINE:-}" ]]; then
+if [[ -z "${BUILDKITE_PIPELINE:-}" ]]; then
   echo "You must set the BUILDKITE_PIPELINE environment variable (e.g. BUILDKITE_PIPELINE = \"my-org/my-pipeline\")"
   exit 1
 fi
@@ -16,6 +16,7 @@ ORG_SLUG=$(echo "${BUILDKITE_PIPELINE}" | cut -d'/' -f1)
 PIPELINE_SLUG=$(echo "${BUILDKITE_PIPELINE}" | cut -d'/' -f2)
 COMMIT="${BUILDKITE_COMMIT:-${GITHUB_SHA}}"
 BRANCH="${BUILDKITE_BRANCH:-${GITHUB_REF#"refs/heads/"}}"
+MESSAGE="${BUILDKITE_MESSAGE:}"
 
 curl \
   -X POST \
@@ -24,6 +25,7 @@ curl \
   -d @- << JSON
 {
   "commit": "${COMMIT}",
-  "branch": "${BRANCH}"
+  "branch": "${BRANCH}",
+  "message": "${MESSAGE}"
 }
 JSON
