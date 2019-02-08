a48404cd23eb181d0e2c2cf0c2ffbd4d9eb030d2
Tim Lucas <t@toolmantim.com>
2019-02-08 15:57:13 +1100

    Remove hack that doesn't work

--- bk/entrypoint.sh
+++ bk/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 set -e
 
-# Workaround until https://github.com/buildkite/cli/pull/30 is merged
-mkdir -p /tmp/buildkite-plugins
-export BUILDKITE_PLUGINS_PATH="/tmp/buildkite-plugins"
-
 exec bk "$@"
